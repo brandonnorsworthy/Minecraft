a25e57987f74b0844e7f1cd3c8d81e6e2ae16d2a
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2019-12-25 10:54:19 -0600

    chunk generation done

--- Source/Minecraft/VoxelActor.cpp
+++ Source/Minecraft/VoxelActor.cpp
@@ -68,7 +68,6 @@ void AVoxelActor::GenerateChunk()
 			for (int32 z = 0; z < chunkZElements; z++)
 			{
 				int32 index = x + (y * chunkLineElements) + (z * chunkLineElementsP2);
-
 				chunkFields[index] = (z < 30) ? 1 : 0;
 			}
 		}
@@ -86,6 +85,113 @@ void AVoxelActor::UpdateMesh()
 
 	int32 elementID = 0;
 
+	for (int32 x = 0; x < chunkLineElements; x++)
+	{
+		for (int32 y = 0; y < chunkLineElements; y++)
+		{
+			for (int32 z = 0; z < chunkZElements; z++)
+			{
+				int32 index = x + (y * chunkLineElements) + (z * chunkLineElementsP2);
+				int32 meshIndex = chunkFields[index];
+
+				if (meshIndex > 0)
+				{
+					meshIndex--;
+
+					int triangle_num = 0;
+					for (int i = 0; i < 6; i++)
+					{
+						int newIndex = index + bMask[i].X + (bMask[i].Y * chunkLineElements) + (bMask[i].Z * chunkLineElementsP2);
+
+						bool flag = false;
+						if (meshIndex >= 20) flag = true;
+						else if ((x + bMask[i].X < chunkLineElements) && (x + bMask[i].X >= 0) && (y + bMask[i].Y < chunkLineElements) && (y + bMask[i].Y >= 0))
+						{
+							if (newIndex < chunkFields.Num() && newIndex >= 0)
+								if (chunkFields[newIndex] < 1) flag = true;
+						}
+						else flag = true;
+
+						if (flag)	
+						{
+							Triangles.Add(bTriangles[0] + triangle_num + elementID);
+							Triangles.Add(bTriangles[1] + triangle_num + elementID);
+							Triangles.Add(bTriangles[2] + triangle_num + elementID);
+							Triangles.Add(bTriangles[3] + triangle_num + elementID);
+							Triangles.Add(bTriangles[4] + triangle_num + elementID);
+							Triangles.Add(bTriangles[5] + triangle_num + elementID);
+							triangle_num += 4; //add 4 verticies to next triangle
+
+							switch (i)
+							{
+							case 0: {
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+
+								Normals.Append(bNormals0, ARRAY_COUNT(bNormals0));
+								break;
+							}
+							case 1: {
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+
+								Normals.Append(bNormals1, ARRAY_COUNT(bNormals1));
+								break;
+							}
+							case 2: {
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+
+								Normals.Append(bNormals2, ARRAY_COUNT(bNormals2));
+								break;
+							}
+							case 3: {
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+
+								Normals.Append(bNormals3, ARRAY_COUNT(bNormals3));
+								break;
+							}
+							case 4: {
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+
+								Normals.Append(bNormals4, ARRAY_COUNT(bNormals4));
+								break;
+							}
+							case 5: {
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), -voxelSizeHalf + (z * voxelSize)));
+								Vertices.Add(FVector(-voxelSizeHalf + (x * voxelSize), -voxelSizeHalf + (y * voxelSize), voxelSizeHalf + (z * voxelSize)));
+
+								Normals.Append(bNormals5, ARRAY_COUNT(bNormals5));
+								break;
+							}
+							}
+
+							UVs.Append(bUVs, ARRAY_COUNT(bUVs));
+
+							FColor color = FColor(255, 255, 255, i);
+							VertexColors.Add(color); VertexColors.Add(color); VertexColors.Add(color); VertexColors.Add(color);
+						}
+					}
+					elementID += triangle_num;
+				}
+			}
+		}
+	}
+	proceduralComponent->ClearAllMeshSections();
 	proceduralComponent->CreateMeshSection(0, Vertices, Triangles, Normals, UVs, VertexColors, Tangents, true);
 }
 
